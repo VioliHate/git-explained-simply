dcb612e30dc4476fdea4dd5c1250550a77f6cda7
Domenico Violi <dome.violi90@gmail.com>
2026-03-20 14:12:06 +0100

    update
    i18n now work +
    use slide data with first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Header } from "./components/Header";
+import { getSlides } from "./data/slides";
 
 export default function App() {
+  const { t } = useTranslation();
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = getSlides(setCurrentSlide, t);
+
+  const nextSlide = () => {
+    if (currentSlide < slides.length - 1) setCurrentSlide(currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    if (currentSlide > 0) setCurrentSlide(currentSlide - 1);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") nextSlide();
+      if (e.key === "ArrowLeft") prevSlide();
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [currentSlide]);
+
   return (
     <div className='min-h-screen flex flex-col overflow-hidden'>
-      <Header currentSlide={0} totalSlides={10} />
+      <Header currentSlide={currentSlide} totalSlides={slides.length} />
     </div>
   );
 }
